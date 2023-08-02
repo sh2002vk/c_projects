470cc56570e66e9de1e1703b539aa70137b60b7c
sh2002vk <sh2002vk@gmail.com>
2023-08-02 14:21:16 -0400

    made draft of the shell

--- shell.c
+++ shell.c
@@ -2,32 +2,56 @@
 #include <stdio.h>
 #include <string.h>
 
+void print_directory
+
+void handle_system_commands(char* commands) {
+    printf("commands s: %s\n", commands);
+}
+
+void handle_file_commands(char* commands){
+    printf("commands f: %s\n", commands);
+}
+
+
+void process_command(char* type, char* commands) {
+    printf("operation: %s\n", type);
+    if (strcmp(type, "file") == 0) {
+        handle_file_commands(commands);
+    } else if (strcmp(type, "system") == 0) {
+        handle_system_commands(commands);
+    } else {
+        printf("INVALID COMMAND CALL\n");
+    }
+}
+
 void shell_loop(){
-    char* input = NULL;
-    size_t len = 0;     // size_t is always positive, used to represent size of objects
+    char* input = malloc(256 * sizeof(char));
+    size_t len = 0;                        // size_t is always positive, used to represent size of objects
 
     while (1) {
-        getline(&input, &len, stdin);   // use this instead of scanf to read whole line at once
-        input[strcspn(input, "\n")] = 0;          // get rid of newline
+        getline(&input, &len, stdin);      // use this instead of scanf to read whole line at once
+        input[strcspn(input, "\n")] = 0;   // get rid of newline
 
         if (strcmp(input, "END") == 0) {
             printf("ENDING PROGRAM\n");
             break;
         } else {
-            printf("%s", input);
-            printf("\n");
+            char* type = strtok(input, " ");
+            char* commands = strtok(NULL, "\n");
+            process_command(type, commands);
         }
     }
+
     free(input);
 }
 
 int main(int argc, char* argv[]){
-    // LOAD FILES
+    // TODO: LOAD FILES
 
     // RUN EXECUTION LOOP
     shell_loop();
 
-    // SHUTDOWN COMMANDS
+    // TODO: SHUTDOWN COMMANDS
 
     return 0;
 }
