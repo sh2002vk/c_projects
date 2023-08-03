6afc8e0e6ab1bfcceff68bb0cfedde0e897de3f6
sh2002vk <sh2002vk@gmail.com>
2023-08-03 17:22:35 -0400

    added operations for files. still need to add systems and going backweards in file directory ability

--- shell.c
+++ shell.c
@@ -1,15 +1,82 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <limits.h>
 
-void print_directory
+void get_current_path(char* path, size_t path_size){
+    getcwd(path, path_size);
+}
+
+int list_directory(char* path){
+    DIR *d;             // a directory object
+    struct dirent *dir; // used to navigate directories
+
+    d = opendir(path);
+    printf("STARTING DIRECTORY\n\n---");
+    if (d) {
+        while ((dir = readdir(d)) != NULL) {
+            printf("%s\n", dir->d_name);
+            printf("---\n");
+        }
+        closedir(d);
+    }
+    printf("\n\nENDING DIRECTORY\n");
+    return 0;
+}
+
+int enter(char* path, char* directory) {
+    strcat(path, directory); // add to path. note that path already has the size of a max directory
+    if (chdir(path) != 0) {
+        return 1;
+    }
+    return 0;
+
+}
 
 void handle_system_commands(char* commands) {
     printf("commands s: %s\n", commands);
 }
 
 void handle_file_commands(char* commands){
-    printf("commands f: %s\n", commands);
+    char path[PATH_MAX];
+    get_current_path(path, sizeof(path));
+    if (path[0] == '\0') {
+        printf("unable to find current path\n");
+        return;
+    }
+
+    // Parse commands for usage by internal functions
+    char* words[256];
+    int wordCount = 0;
+
+    char* word = strtok(commands, " ");
+    while(word != NULL) {
+        words[wordCount++] = word;
+        word = strtok(NULL, " ");
+    }
+
+    if (wordCount == 0) {
+        printf("No operation provided\n");
+        return;
+    }
+
+    // Handle the operations
+    if (!strcmp(words[0], "list")) {
+        list_directory(path);
+    } else if (!strcmp(words[0], "print")) {
+        printf("current path: %s\n", path);
+    } else if (!strcmp(words[0], "enter")) {
+        if (enter(path, words[1]) != 0) {  // words [1] is the directory we're going into
+            printf("failed to enter directory %s\n", words[1]);
+        } else {
+            get_current_path(path, sizeof(path));   // update the current path
+            printf("changed to directory: %s\n",path);
+        }
+    } else {
+        printf("invalid operation\n");
+    }
 }
 
 
